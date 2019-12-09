8ddc98b40065f89aed8c981367ceb04ef9baf024
coltborg <coltborg@gmail.com>
2019-12-09 00:05:26 -0600

    Add social meta

--- src/main.js
+++ src/main.js
@@ -6,6 +6,46 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
+  // Google / Facebook
+  head.meta.push({
+    property: 'og:type',
+    content: 'website'
+  })
+  head.meta.push({
+    property: 'og:url',
+    content: 'https://www.coltborg.com/'
+  })
+  head.meta.push({
+    property: 'og:title',
+    content: 'Colt Borg'
+  })
+  head.meta.push({
+    property: 'og:description',
+    content: 'Web developer, with a love for cats.'
+  })
+  head.meta.push({
+    property: 'og:image',
+    content: 'https://www.coltborg.com/social.png'
+  })
+
+  // Twitter
+  head.meta.push({
+    property: 'twitter:card',
+    content: 'summary_large_image'
+  })
+  head.meta.push({
+    property: 'twitter:url',
+    content: 'https://www.coltborg.com/'
+  })
+  head.meta.push({
+    property: 'twitter:description',
+    content: 'Web developer, with a love for cats.'
+  })
+  head.meta.push({
+    property: 'twitter:image',
+    content: 'https://www.coltborg.com/social.png'
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
